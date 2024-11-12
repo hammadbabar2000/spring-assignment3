0ebf7b0882945d5cddd3f60de2c0dcb8ecb346ba
Hammad Babar <hammadbabar2000@outlook.com>
2024-11-12 17:51:08 -0500

    commit

---
+++ src/main/java/ca/sheridancollege/babarha/lec92_customformlogin/beans/User.java
@@ -0,0 +1,25 @@
+package ca.sheridancollege.babarha.lec92_customformlogin.beans;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+
+public class User {
+
+    private Long userId;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String encryptedPassword;
+
+    @NonNull
+    private Boolean enabled;
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO sec_user (email, encryptedPassword, enabled)
+VALUES ('fahad.jan@sheridancollege.ca', '$2a$10$1ltibqiyyBJMJQ4hqM7f0OusP6np/IHshkYc4TjedwHnwwNChQZCy', 1);
+
+INSERT INTO sec_user (email, encryptedPassword, enabled)
+VALUES ('frank@sheridancollege.ca', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
+
+INSERT INTO sec_user (email, encryptedPassword, enabled)
+VALUES ('admin@sheridancollege.ca', '$2a$10$gnyPemF6mcQXbWz1TOTlTufqLdzVrkOjSjWkRrDJSPk7Nq7rVp9aO', 1);
+
+
+INSERT INTO sec_role (roleName)
+VALUES ('ROLE_USER');
+ 
+INSERT INTO sec_role (roleName)
+VALUES ('ROLE_GUEST');
+
+INSERT INTO sec_role (roleName)
+VALUES ('ROLE_ADMIN');
+ 
+
+ 
+INSERT INTO user_role (userId, roleId)
+VALUES (1, 1);
+ 
+INSERT INTO user_role (userId, roleId)
+VALUES (2, 2);
+
+INSERT INTO user_role (userId, roleId)
+VALUES (3, 3);
